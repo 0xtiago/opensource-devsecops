a819ba9102b4820182771b7f548474215e9e6424
Tiago Tavares <tiagotvrs@gmail.com>
2024-07-22 10:42:35 -0300

    c

--- tools/horusec/horusec.sh
+++ tools/horusec/horusec.sh
@@ -8,7 +8,7 @@ mkdir reports
 image="horuszup/horusec-cli:v2.9.0-beta.3"
 severity_exception="LOW,UNKNOWN,INFO"
 report_type="text"
-#report_path="reports/horusec_report.json"
+report_path="reports/horusec_report.json"
 ignore="**/tmp/**,
 	      **/.vscode/**,\
 				**/.venv/**, \
@@ -32,8 +32,9 @@ docker run --rm \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $(pwd):/src/horusec $image horusec start \
 	-p /src/horusec -P $(pwd) \
-	-o="$report_type" -O=/src/horusec/$report_path \
+	-o="$report_type" \
 	-s=$severity_exception \
 	--ignore=$ignore \
-	--information-severity=true
+	--information-severity=true \
+	#-O=/src/horusec/$report_path
 	
