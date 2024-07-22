7265f77e413477aa74a3ae3ac29a8e623c3d15ed
Tiago Tavares <tiagotvrs@gmail.com>
2024-07-22 14:41:19 -0300

    Horusec Linux e Windows

---
+++ tools/horusec/horusec_windows.ps1
@@ -0,0 +1,40 @@
+# CONFIGURAÇÃO DE PARÂMETROS DO HORUSEC
+$image = "horuszup/horusec-cli:v2.9.0-beta.3"
+$severity_exception = "LOW,UNKNOWN,INFO"
+$report_type = "json"
+$report_directory = "reports"
+$report_path = "reports/horusec_report.json"
+$ignore = @(
+    "**/tmp/**",
+    "**/.vscode/**",
+    "**/.venv/**",
+    "**/.env/**",
+    "**/tests/**",
+    "**/test/**",
+    "**/test/",
+    "**/*.Tests/**",
+    "**/*.Test/**",
+    "**/test_*",
+    "**/appsettings.*.json",
+    "**/bin/Debug/*/appsettings.*.json",
+    "**/*.yml",
+    "**/bin/Debug/*/appsettings.json",
+    "**/*.sarif"
+) -join ','
+
+# CRIA PASTA DE RELATÓRIO
+if (-Not (Test-Path -Path $report_directory)) {
+    New-Item -ItemType Directory -Path $report_directory
+}
+
+# EXECUTA CONTAINER DO HORUSEC REMOVENDO-O AO FIM DA EXECUÇÃO
+docker pull $image
+docker run --rm `
+    -v /var/run/docker.sock:/var/run/docker.sock `
+    -v ${PWD}:/src/horusec $image horusec start `
+    -p /src/horusec -P ${PWD} `
+    -o="$report_type" `
+    -O="$report_directory/$report_path" `
+    -s="$severity_exception" `
+    --ignore="$ignore" `
+    --information-severity=$true
