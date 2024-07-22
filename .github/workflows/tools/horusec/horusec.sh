47b1aadd8ada837ffa98fdad6dfe9eca61ca7f67
Tiago Tavares <tiagotvrs@gmail.com>
2024-07-22 10:06:24 -0300

    fix

---
+++ .github/workflows/tools/horusec/horusec.sh
@@ -0,0 +1,39 @@
+mkdir reports
+
+#CRIA PASTA DE RELATÓRIO
+mkdir reports
+
+#CONFIGURAÇÃO DE PARÂMETROS DO HORUSEC
+#image="0xtiago/horusec-cli"
+image="horuszup/horusec-cli:v2.9.0-beta.3"
+severity_exception="LOW,UNKNOWN,INFO"
+report_type="json"
+report_path="reports/horusec_report.json"
+ignore="**/tmp/**,
+	      **/.vscode/**,\
+				**/.venv/**, \
+				**/.env/**, \
+				**/tests/**, \
+				**/test/**, \
+				**/test/, \
+				**/*.Tests/**, \
+				**/*.Test/**, \
+				**/test_*, \
+				**/appsettings.*.json, \
+				**/bin/Debug/*/appsettings.*.json, \
+				**/*.yml, \
+				**/bin/Debug/*/appsettings.json, \
+				**/*.sarif" ;\
+
+
+# EXECUTA CONTAINER DO HORUSEC REMOVENDO-O AO FIM DA EXECUÇÃO
+docker pull $image
+docker run --rm \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v $(pwd):/src/horusec $image horusec start \
+	-p /src/horusec -P $(pwd) \
+	-o="$report_type" -O=/src/horusec/$report_path \
+	-s=$severity_exception \
+	--ignore=$ignore \
+	--information-severity=true
+	
